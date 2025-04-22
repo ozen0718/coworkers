b9dbfe84b5dbe64190f25efe45bf5992743b497d
gyoyeon Kim <gy2121@naver.com>
2025-04-22 17:22:36 +0900

    [Img] kebab icon 추가

---
+++ src/app/card/page.tsx
@@ -0,0 +1,11 @@
+import BoardComment from "@/components/Card/Comment/BoardComment";
+
+export default function Test() {
+  return (
+    <div className="min-h-screen p-10 bg-white">
+      <h1 className="text-xl-semibold text-blue">card Test Page</h1>
+      <br />
+      <BoardComment />
+    </div>
+  );
+}
---
+++ src/components/Card/Comment/BoardComment.tsx
@@ -0,0 +1,10 @@
+export default function BoardComment() {
+  return (
+    <div className="flex flex-col  sm:w-[343px] md:w-[1200px] sm:h-[113px] bg-bg200 p-5">
+      {/* 댓글 내용 */}
+      <div className="flex flex-col sm:flex-row items-center sm:items-start">
+        댓글 영역입니다.
+      </div>
+    </div>
+  );
+}
