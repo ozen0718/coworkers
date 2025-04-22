4ad9766ca7ad53972cbdf27608ea5adbe02a0ea8
gyoyeon Kim <gy2121@naver.com>
2025-04-23 00:59:39 +0900

    [Feat] kebab icon 추가

--- src/components/Card/Comment/BoardComment.tsx
+++ src/components/Card/Comment/BoardComment.tsx
@@ -1,11 +1,13 @@
-import Image from "next/image";
+import Kebab from "@/assets/icons/kebab";
 
 export default function BoardComment() {
   return (
-    <div className="flex flex-col lg:w-[1200px] lg:h-[123px] bg-bg200 p-5">
+    <div className="flex flex-col lg:w-[1200px] lg:h-[123px] bg-bg200 p-5 rounded-lg ">
       {/* 댓글 내용 + 아이콘 */}
-      <div className="flex flex-col sm:flex-row items-center sm:items-start">
-        댓글 영역입니다.
+
+      <div className="flex justify-between items-center w-full text-base">
+        <span className="truncate">댓글 영역입니다.</span>
+        <Kebab width={16} height={16} color="#64748B" />
       </div>
     </div>
   );
