f5b0e013a8c141f1e7b2e663b3cbf48a53b1ade6
gyoyeon Kim <gy2121@naver.com>
2025-04-22 23:01:18 +0900

    [Img] 프로필 이미지 추가

--- src/components/Card/Comment/BoardComment.tsx
+++ src/components/Card/Comment/BoardComment.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
+
 export default function BoardComment() {
   return (
-    <div className="flex flex-col  sm:w-[343px] md:w-[1200px] sm:h-[113px] bg-bg200 p-5">
-      {/* 댓글 내용 */}
+    <div className="flex flex-col lg:w-[1200px] lg:h-[123px] bg-bg200 p-5">
+      {/* 댓글 내용 + 아이콘 */}
       <div className="flex flex-col sm:flex-row items-center sm:items-start">
         댓글 영역입니다.
       </div>
