4e072e8d92121e26de8295eb9c4b1f32d5536df3
gyoyeon Kim <gy2121@naver.com>
2025-04-23 15:38:02 +0900

    [Feat] 댓글 내용 컴포넌트 완료

--- src/components/Card/Comment/AuthorInfo.tsx
+++ src/components/Card/Comment/AuthorInfo.tsx
@@ -9,7 +9,7 @@ export default function AuthorInfo({
   showLike = true,
   authorName = "우지은",
   date = "2024.07.25",
-  profileSpacing = "6px", // 프로필, 이름, 선 간격
+  profileSpacing = "7px", // 프로필, 이름, 선 간격
 }) {
   const [isLiked, setIsLiked] = useState(false);
 
@@ -18,8 +18,11 @@ export default function AuthorInfo({
   };
 
   return (
-    <div className="flex items-center w-full text-xs mt-3 justify-between">
-      <div className="flex items-center" style={{ columnGap: profileSpacing }}>
+    <div className="flex items-center w-full mt-3 justify-between sm:text-md-medium not-last:sm:flex-nowrap">
+      <div
+        className="flex items-center sm:flex-nowrap"
+        style={{ columnGap: profileSpacing }}
+      >
         <Image
           src="/icons/initialprofile.svg"
           alt="프로필 사진"
@@ -34,7 +37,7 @@ export default function AuthorInfo({
       </div>
 
       {showLike && (
-        <div className="flex items-center">
+        <div className="flex items-center ml-2">
           <Image
             className="cursor-pointer mr-1"
             src={isLiked ? "/icons/heart-filled.svg" : "/icons/heart.svg"}
--- src/components/Card/Comment/BoardComment.tsx
+++ src/components/Card/Comment/BoardComment.tsx
@@ -4,14 +4,11 @@ import AuthorInfo from "./AuthorInfo";
 export default function BoardComment() {
   return (
     <div className="flex flex-col w-full lg:w-[1200px] lg:h-[123px] min-h-[113px] bg-bg200 p-5 rounded-lg">
-      <div className="flex justify-between items-start w-full text-base">
+      <div className="flex justify-between items-start w-full text-lg-regular">
         <div className="flex items-start w-full relative">
           <span className="block min-h-[40px] max-h-[50px] overflow-y-auto overflow-x-hidden scroll-area mr-2 pr-6">
-            댓글 영역입니다. 댓글 영역입니다. 댓글 영역입니다. 댓글 영역입니다.
-            댓글 영역입니다. 댓글 영역입니다. 댓글 영역입니다. 댓글 영역입니다.
-            댓글 영역입니다. 댓글 영역입니다. 댓글 영역입니다. 댓글 영역입니다.
+            댓글 영역입니다.
           </span>
-
           <Image
             className="absolute right-0 top-0 cursor-pointer"
             src="/icons/kebab.svg"
