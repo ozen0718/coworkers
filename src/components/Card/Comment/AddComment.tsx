f05405b68312e9011134cbedfbf631e43c7b5b99
gyoyeon Kim <gy2121@naver.com>
2025-04-23 17:33:47 +0900

    [Style] global.css 피그마에 맞게 수정

--- src/components/Card/Comment/AddComment.tsx
+++ src/components/Card/Comment/AddComment.tsx
@@ -2,12 +2,17 @@ import Button from "@/components/Button/Button";
 
 export default function AddComment() {
   return (
-    <div className="flex flex-col w-full lg:w-[1200px] lg:h-[216px] min-h-[113px] bg-bg200 p-5 rounded-lg">
-      <div className="flex justify-between items-start w-full text-lg-regular">
+    <div className="flex flex-col w-full lg:w-[1200px] lg:h-[216px] min-h-[113px] bg-trasparent p-5 rounded-lg bg-black">
+      <div className="flex justify-between items-start w-full text-xl-bold text-gray100">
         댓글달기
       </div>
-      <div className="flex items-start w-full relative">
+      <div className="mt-6 bg-bg200 flex items-start w-full relative">
         댓글 입력 텍스트에리아
+      </div>
+      <div>
+        {" "}
+        <Button size="large" />
+        버튼
       </div>
     </div>
   );
