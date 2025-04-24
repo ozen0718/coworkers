4dd740971226ab234cc363bdfab89690691a587d
heresue <sue.synpark@gmail.com>
2025-04-25 04:48:24 +0900

    [Style] 임시 페이지 background color 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="bg-bg200 min-h-screen p-10">
+    <div className="bg-bg300 min-h-screen p-10">
       <h1 className="text-xl-semibold text-blue">Test Page</h1>
       <span>test</span>
     </div>
