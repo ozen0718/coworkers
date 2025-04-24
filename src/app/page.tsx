927a040c0c35f27547814b726e222b1d04b0f98b
ozen0718 <limoyng301p@naver.com>
2025-04-24 11:36:16 +0900

    [Fix] 테스트코드 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,7 @@
-'use client';
-
-import React, { useEffect, useState } from 'react';
-import SideMenu from '@/components/Gnb/SideMenu';
-import Header from '@/components/Gnb/Header';
-import { HeaderProvider, useHeader } from '@/components/Gnb/HeaderContext';
-
-const USER_DATA = {
-  name: '안혜나',
-  teams: [
-    { id: 1, name: '경영관리팀' },
-    { id: 2, name: '프로젝트팀' },
-  ],
-};
-
-function PageContent() {
-  const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
-  const { setHeaderState } = useHeader();
-
-  useEffect(() => {
-    setHeaderState({
-      showTeamSelector: true, //팀선택 드롭다운
-      showFreeBoardLink: true, //자유게시판
-      showProfile: true, //프로필
-    });
-  }, []);
-
+export default function Home() {
   return (
-    <div className="bg-bg200 min-h-screen text-white">
-      <Header onOpenSideMenu={() => setIsSideMenuOpen(true)} />
-      <SideMenu
-        teams={USER_DATA.teams}
-        isOpen={isSideMenuOpen}
-        onClose={() => setIsSideMenuOpen(false)}
-      />
+    <div className="bg-bg200 min-h-screen p-10">
+      <h1 className="text-xl-semibold text-blue">Test Page</h1>
     </div>
-  );
-}
-
-export default function TestPage() {
-  return (
-    <HeaderProvider>
-      <PageContent />
-    </HeaderProvider>
   );
 }
