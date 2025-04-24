67e949ad6d13fec60b358bbc69231ea58c31c82c
gyoyeon Kim <gy2121@naver.com>
2025-04-24 12:19:09 +0900

    [Style] Page 색상 bg-300으로 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import AddComment from '@/components/Card/Comment/AddComment';
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-white p-10">
+    <div className="bg-bg300 min-h-screen p-10">
       <h1 className="text-xl-semibold text-blue">card Test Page</h1>
       <br />
       <BoardComment />
