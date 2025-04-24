71c900aa6c1e8aaf413907370f6aabc3770b12cf
ozen0718 <limyong301p@naver.com>
2025-04-24 03:10:48 +0000

    [Img] kebab icon 추가

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="min-h-screen p-10 bg-bg200">
-      <h1 className="text-xl-semibold text-blue">Test Page</h1>
-    </div>
-  );
-}
---
+++ src/components/Card/Comment/AuthorInfo.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import Image from 'next/image';
+import { useState } from 'react';
+import IconHeart from '@/assets/icons/IconHeart';
+
+// 임시 데이터
+export default function AuthorInfo({
+  showDivider = true, // 선 유무
+  showLike = true,
+  authorName = '우지은',
+  date = '2024.07.25',
+  profileSpacing = '7px', // 프로필, 이름, 선 간격
+}) {
+  const [isLiked, setIsLiked] = useState(false);
+
+  const toggleLike = () => {
+    setIsLiked(!isLiked);
+  };
+
+  return (
+    <div className="mt-3 flex w-full items-center justify-between text-[10px] sm:text-sm">
+      <div className="flex items-center sm:flex-nowrap" style={{ columnGap: profileSpacing }}>
+        <Image src="/icons/initialprofile.svg" alt="프로필 사진" width={32} height={32} />
+        <span className="block">{authorName}</span>
+        {showDivider && <div className="h-[12px] border-l-2 border-slate-700"></div>}
+        <span className="text-gray400">{date}</span>
+      </div>
+
+      {showLike && (
+        <div className="ml-2 flex items-center">
+          <IconHeart
+            className="mr-1 cursor-pointer"
+            fillColor={isLiked ? '#EF4444' : 'none'}
+            strokeColor={isLiked ? '#EF4444' : '#64748B'}
+            onClick={toggleLike}
+          />
+          <span className="text-gray400">9999+</span>
+        </div>
+      )}
+    </div>
+    /**
+     * @fixme api 연결 후 좋아요 개수, 좋아요 여부 데이터로 변경
+     */
+  );
+}
