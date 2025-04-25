1e8119f0229dfc99ffcb1f807de475d1ab9946a9
gyoyeon Kim <gy2121@naver.com>
2025-04-25 16:37:30 +0900

    [Refactor] 말줄임표 테스트 best, general

--- src/components/Card/Post/BestPost.tsx
+++ src/components/Card/Post/BestPost.tsx
@@ -4,7 +4,7 @@ export function BestPost() {
   return (
     <PostCard
       type="best"
-      title="자유게시판에 질문을 올릴 수 있어요 질문을 올려볼까요dfasdfadfadfadf?"
+      title="자유게시판에 질문을 올릴 수 있어요 질문을 올려볼까요?"
       imgUrl="https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Coworkers/user/1855/testimage.jpg"
       date="2024.07.25"
     />
--- src/components/Card/Post/GeneralPost.tsx
+++ src/components/Card/Post/GeneralPost.tsx
@@ -14,7 +14,7 @@ export function GeneralPost() {
     <PostCard
       type="general"
       size={size}
-      title="자유게시판에자유게시판에자유게시판에자유게시판에자유게시판에자유게시판에dddddddddddddddddddd안녕자고 싶다잠온다몇시지 이이이 "
+      title="자유게시판에 질문을 올릴 수 있어요. 질문을 올려볼까요? 말줄임표 테스트 되는지 볼까요? 잠와요"
       imgUrl="https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Coworkers/user/1855/testimage.jpg"
       date="2024.07.25"
       showKebab={size == 'small'}
