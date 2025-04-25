755b6cfd07556c129f131eac81394a9356ed1a3e
gyoyeon Kim <gy2121@naver.com>
2025-04-25 16:22:02 +0900

    [Remove] page.tsx : 테스트 코드 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,8 @@
-import BoardComment from '@/components/Card/Comment/BoardComment';
-import AddComment from '@/components/Card/Comment/AddComment';
-import ImgUpload from '@/components/Card/ImgUpload';
-
-import { BestPost } from '@/components/Card/Post/BestPost';
-import { GeneralPost } from '@/components/Card/Post/GeneralPost';
-
-import { DateInfo } from '@/components/Card/Post/Deatil/DateInfo';
-
 export default function Home() {
   return (
     <div className="bg-bg300 min-h-screen p-10">
-      <h1 className="text-xl-semibold text-blue">card Test Page</h1>
-      <br />
-      <BoardComment />
-      <br />
-      <AddComment />
-      <br />
-      <ImgUpload />
-
-      <br />
-      <BestPost />
-      <br />
-      <br />
-      <GeneralPost />
-
-      <DateInfo date="2025-04-25T00:00:00Z" time="15:30" repeatinfo="DAILY" />
+      <h1 className="text-xl-semibold text-blue">Test Page</h1>
+      <span>test</span>
     </div>
   );
 }
