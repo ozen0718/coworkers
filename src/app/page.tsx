b4594c0a8fc73c6edbdf432bc25ddc0526696f8f
gyoyeon Kim <gy2121@naver.com>
2025-04-25 14:53:20 +0900

    [Test] page.tsx 테스트 코드 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,27 @@
+import BoardComment from '@/components/Card/Comment/BoardComment';
+import AddComment from '@/components/Card/Comment/AddComment';
+import ImgUpload from '@/components/Card/ImgUpload';
+//import { BestPost, GeneralPost } from '@/components/Card/BoardPost';
+
+import { BestPost } from '@/components/Card/Post/BestPost';
+import { GeneralPost } from '@/components/Card/Post/GeneralPost';
+
 export default function Home() {
   return (
     <div className="bg-bg300 min-h-screen p-10">
-      <h1 className="text-xl-semibold text-blue">Test Page</h1>
-      <span>test</span>
+      <h1 className="text-xl-semibold text-blue">card Test Page</h1>
+      <br />
+      <BoardComment />
+      <br />
+      <AddComment />
+      <br />
+      <ImgUpload />
+
+      <br />
+      <BestPost />
+      <br />
+      <br />
+      <GeneralPost />
     </div>
   );
 }
