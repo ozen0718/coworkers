cbbfc1c1251cb9d8992384a5085b09c696449f76
gyoyeon Kim <gy2121@naver.com>
2025-04-30 01:14:48 +0900

    [Feat] 자유게시판 검색 input 생성

--- src/app/(main)/boards/[articleid]/page.tsx
+++ src/app/(main)/boards/[articleid]/page.tsx
@@ -1,13 +1,5 @@
 'use client';
 
-import { HeaderProvider } from '@/components/layout/Gnb/HeaderContext';
-import Header from '@/components/layout/Gnb/Header';
-
 export default function ArticleDetail() {
-  return (
-    <HeaderProvider>
-      <Header onOpenSideMenu={() => {}} />
-      <div>자유게시판 - 상세페이지</div>
-    </HeaderProvider>
-  );
+  return <div>자유게시판 - 상세페이지</div>;
 }
--- src/app/(main)/boards/page.tsx
+++ src/app/(main)/boards/page.tsx
@@ -3,8 +3,23 @@
 export default function BoardPage() {
   return (
     <div className="bg-bg300 flex min-h-screen flex-col items-center text-white">
-      <div className="w-full max-w-[1200px] px-4 sm:px-6 md:px-4">
-        <h1 className="text-2xl-bold text-gray100 mt-10 w-full">자유게시판</h1>
+      <div className="w-full max-w-[1200px] px-4">
+        <div className="w-full max-w-[1200px]">
+          <h1 className="text-2xl-bold text-gray100 mt-10">자유게시판</h1>
+        </div>
+
+        <div className="relative mt-10 w-full max-w-[1201px]">
+          <img
+            src="/icons/search.svg"
+            alt="검색 아이콘"
+            className="absolute top-1/2 left-4 h-5 w-5 -translate-y-1/2 cursor-pointer"
+          />
+          <input
+            type="text"
+            placeholder="검색어를 입력해주세요"
+            className="bg-bg200 w-full rounded-xl border border-[#F8FAFC1A] p-4 pl-12"
+          />
+        </div>
       </div>
     </div>
   );
