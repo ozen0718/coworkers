1e77b99c93ea3db23e2d60c021c09a7a163eb534
gyoyeon Kim <gy2121@naver.com>
2025-04-29 16:12:10 +0900

    [Feat] 자유게시판 페이지 추가

---
+++ src/app/(main)/boards/[articleid]/page.tsx
@@ -0,0 +1,3 @@
+export default function ArticleDeatil() {
+  return <div className="bg-bg300 min-h-screen p-10">자유게시판 상세페이지</div>;
+}
--- src/app/(main)/boards/page.tsx
+++ src/app/(main)/boards/page.tsx
@@ -1,3 +1,3 @@
 export default function BoardsPage() {
-  return <div>Boards Page</div>;
+  return <div className="bg-bg300 min-h-screen p-10">Boards Page</div>;
 }
