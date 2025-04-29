b6dd982af723c549dd2809295de8c1e78907c98b
gyoyeon Kim <gy2121@naver.com>
2025-04-29 22:38:01 +0900

    [Fea] 자유게시판 타이틀

--- src/app/(main)/boards/page.tsx
+++ src/app/(main)/boards/page.tsx
@@ -5,8 +5,14 @@ import Header from '@/components/layout/Gnb/Header';
 
 export default function BoardPage() {
   return (
-    <HeaderProvider>
-      <Header onOpenSideMenu={() => {}} />
-    </HeaderProvider>
+    <div className="bg-bg300 flex min-h-screen flex-col items-center text-white">
+      <HeaderProvider>
+        <Header onOpenSideMenu={() => {}} />
+        {/* 여기만 위에 여백 줄 것 */}
+        <div className="text-2xl-bold text-gray100 mt-10 flex h-full max-h-[1196px] w-full max-w-[1201px]">
+          자유게시판
+        </div>
+      </HeaderProvider>
+    </div>
   );
 }
