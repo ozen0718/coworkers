b7570c33fc904de7c23eb79f7f1b2dace0e318de
gyoyeon Kim <gy2121@naver.com>
2025-05-01 15:44:53 +0900

    [Feat] 게시글 쓰기 : 타이틀 + 버튼

--- src/app/(main)/boards/new/page.tsx
+++ src/app/(main)/boards/new/page.tsx
@@ -1,9 +1,22 @@
 'use client';
+import Button from '@/components/common/Button/Button';
 
 export default function CreateBoard() {
   return (
-    <div className="text-gray300 flex flex-col items-center">
-      <div className="w-full max-w-[1200px] px-4">안녕하세요</div>
+    <div className="text-gray100 flex flex-col items-center">
+      <div className="max-h-[841px] w-full max-w-[1200px] px-5">
+        {/* 타이틀 + 버튼 */}
+        <div className="w-full flex-col">
+          <div className="mt-15 flex w-full max-w-[1200px] items-center justify-between">
+            <p className="text-xl-bold flex">게시글 쓰기</p>
+            <div className="relative">
+              <button className="text-lg-semibold bg-primary hover:bg-primary-hover active:bg-primary-pressed max-[620px]:text-md-semibold flex h-12 w-[184px] items-center justify-center rounded-xl text-white max-[620px]:h-8 max-[620px]:w-[100px]">
+                등록
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/(main)/boards/page.tsx
+++ src/app/(main)/boards/page.tsx
@@ -27,7 +27,7 @@ export default function BoardPage() {
 
   return (
     <div className="bg-bg300 flex min-h-screen flex-col items-center text-white">
-      <div className="w-full max-w-[1200px] px-4">
+      <div className="w-full max-w-[1200px] px-5">
         <div className="w-full max-w-[1200px]">
           <p className="text-gray100 mt-10 font-bold sm:text-2xl">자유게시판</p>
         </div>
