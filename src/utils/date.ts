2218edfe175082ba9039b6fe6ef973d1e55cb323
ozen0718 <limyong301p@naver.com>
2025-05-01 12:19:58 +0000

    [Refactor] Remove unnecessary wrapper div

---
+++ src/utils/date.ts
@@ -0,0 +1,7 @@
+export default function getKoreanDateString(dateString: string): string {
+  const date = new Date(dateString);
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  return `${year}년 ${month}월 ${day}일`;
+}
