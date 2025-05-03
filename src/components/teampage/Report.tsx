73e5da5b8fa030c772fa617d8f0bb03c778f3906
doctor-taco <100111506+doctor-taco@users.noreply.github.com>
2025-05-03 20:14:52 +0900

    [Feat] Add the urgent task elements in the report section

--- src/components/teampage/Report.tsx
+++ src/components/teampage/Report.tsx
@@ -1,28 +1,30 @@
 import Image from 'next/image';
 import ReportProgress from '@/components/teampage/ReportProgress';
-import { ProgressProp } from '@/types/teampagetypes';
+import { ProgressProp, UrgentTaskProps } from '@/types/teampagetypes';
+import Alert from '@/assets/icons/Alert';
 
-const rightSideBoxStyle =
-  'flex sm:gap-6 gap-1 h-fit w-full items-end justify-between bg-bg100 p-4 rounded-xl';
-const rightSideTextBoxStyle = 'flex flex-col items-start justify-center gap-1 ';
-const rightSideLabelStyle = 'text-xs-medium text-gray300';
-const rightSideNumberStyle = 'text-2xl-bold text-tertiary';
+const taskReportBoxStyle = 'flex h-20 w-full items-end justify-between bg-bg100 p-4 rounded-xl';
+const taskReportTextBoxStyle = 'flex flex-col items-start justify-center gap-1 ';
+const taskReportLabelStyle = 'text-xs-medium text-gray300';
+const taskReportNumberStyle = 'text-2xl-bold text-tertiary';
 
 function LeftSide({ percentage }: ProgressProp) {
   return (
-    <div className="flex items-center justify-start gap-6 md:gap-11">
-      <div className="h-43 w-43">
+    <div className="flex w-full items-center justify-start gap-2">
+      <div className="h-43 w-43 shrink-0">
         <ReportProgress percentage={percentage} />
       </div>
-      <div className="hidden flex-col items-start justify-center gap-1 sm:flex">
-        <p className="text-md-medium text-gray100">
-          오늘의
-          <br />
-          진행 상황
-        </p>
-        <p className="from-primary to-tertiary text-4xl-bold bg-gradient-to-r bg-clip-text text-transparent">
-          {percentage}%
-        </p>
+      <div className="flex w-full justify-center">
+        <div className="hidden flex-col items-start justify-center gap-1 lg:flex">
+          <p className="text-md-medium text-gray100">
+            오늘의
+            <br />
+            진행 상황
+          </p>
+          <p className="from-primary to-tertiary text-4xl-bold bg-gradient-to-r bg-clip-text text-transparent">
+            {percentage}%
+          </p>
+        </div>
       </div>
     </div>
   );
@@ -30,10 +32,10 @@ function LeftSide({ percentage }: ProgressProp) {
 
 function TodayTasks() {
   return (
-    <div className={rightSideBoxStyle}>
-      <div className={rightSideTextBoxStyle}>
-        <h3 className={rightSideLabelStyle}>오늘의 할 일</h3>
-        <p className={rightSideNumberStyle}>20개</p>
+    <div className={taskReportBoxStyle}>
+      <div className={taskReportTextBoxStyle}>
+        <h3 className={taskReportLabelStyle}>오늘의 할 일</h3>
+        <p className={taskReportNumberStyle}>20개</p>
       </div>
       <Image src="/icons/head.svg" alt="오늘의 할 일" width={40} height={40} />
     </div>
@@ -42,30 +44,83 @@ function TodayTasks() {
 
 function TodayCompletedTasks() {
   return (
-    <div className={rightSideBoxStyle}>
-      <div className={rightSideTextBoxStyle}>
-        <h3 className={rightSideLabelStyle}>한 일</h3>
-        <p className={rightSideNumberStyle}>5</p>
+    <div className={taskReportBoxStyle}>
+      <div className={taskReportTextBoxStyle}>
+        <h3 className={taskReportLabelStyle}>한 일</h3>
+        <p className={taskReportNumberStyle}>5</p>
       </div>
       <Image src="/icons/sign_done.svg" alt="한 일" width={40} height={40} />
     </div>
   );
 }
 
-function RightSide() {
+function TaskReportColumn() {
   return (
-    <div className="flex w-full max-w-[50%] min-w-fit flex-col gap-4 sm:max-w-[45%] lg:max-w-[33%]">
+    <div className="flex w-full flex-col gap-4">
       <TodayTasks />
       <TodayCompletedTasks />
     </div>
   );
 }
 
+function UrgentTask({ title, dueDate }: UrgentTaskProps) {
+  return (
+    <div
+      className={`${taskReportBoxStyle} border-danger from-danger via-bg200 to-bg100 bg-gradient-to-r via-[12px] to-50%`}
+    >
+      <div className={taskReportTextBoxStyle}>
+        <h3 className="text-xs-medium text-pink">마감임박</h3>
+        <p className={taskReportLabelStyle}>{dueDate}</p>
+        <p className={`${taskReportNumberStyle} text-md-medium break-word line-clamp-1`}>{title}</p>
+      </div>
+    </div>
+  );
+}
+
+function UrgentTaskForMobile({ title, dueDate }: UrgentTaskProps) {
+  return (
+    <div className="bg-bg200 right-[28%] flex h-14 w-full items-center justify-start gap-4 rounded-xl pr-2">
+      <div className="from-danger to-bg200 flex h-full w-70 items-center justify-start gap-2 rounded-l-xl bg-gradient-to-r px-2">
+        <Alert className="text-white" />
+        <div className="flex flex-col items-center justify-center">
+          <p className="text-xs-regular text-tertiary whitespace-nowrap">마감 임박</p>
+          <p className="text-lg-medium text-gray100 h-fit w-fit rounded-l-full whitespace-nowrap">
+            {dueDate}
+          </p>
+        </div>
+      </div>
+      <div className="flex h-full w-full items-center">
+        <p className="text-md-regular break-word line-clamp-2">{title}</p>
+      </div>
+    </div>
+  );
+}
+
+function UrgentTasksReportColumn() {
+  return (
+    <div className="flex w-full flex-col gap-4">
+      <UrgentTask title="주주명부 작성하기" dueDate="15시간 1분 9초" />
+      <UrgentTask title="법인 사무실 임대차 계약 체결하기" dueDate="39시간 33분 56초" />
+    </div>
+  );
+}
+
 export default function Report() {
   return (
-    <div className="bg-bg200 flex h-fit w-full items-center justify-between gap-2 rounded-xl p-2 sm:p-6">
-      <LeftSide percentage={25} />
-      <RightSide />
+    <div className="relative w-full">
+      <div className="bg-bg200 grid grid-cols-2 gap-4 rounded-xl p-2 sm:grid-cols-3">
+        <LeftSide percentage={25} />
+        <div className="hidden justify-center sm:flex">
+          <UrgentTasksReportColumn />
+        </div>
+        <div className="flex justify-end">
+          <TaskReportColumn />
+        </div>
+      </div>
+      <div className="mt-4 flex flex-col items-center justify-start gap-4 sm:hidden">
+        <UrgentTaskForMobile title="주주명부 작성하기" dueDate="15시간 1분 9초" />
+        <UrgentTaskForMobile title="법인 사무실 임대차 계약 체결하기" dueDate="39시간 33분 56초" />
+      </div>
     </div>
   );
 }
