317063ac92d091c2ca68e9ca0a19db80da2bc63f
heresue <sue.synpark@gmail.com>
2025-05-04 07:17:39 +0900

    [Refactor] Edit-page 관심사 분리 및 mockData 추가

--- src/app/(main)/[teamid]/edit/page.tsx
+++ src/app/(main)/[teamid]/edit/page.tsx
@@ -1,8 +1,13 @@
-import Button from '@/components/common/Button/Button';
-import { TextInput } from '@/components/common/Inputs';
-import { EditableTeamProfile } from '@/components/common/Profiles';
+import EditableTeamProfileSection from './EditableTeamProfileSection';
+import EditableTeamNameSection from './EditableTeamNameSection';
+import EditButton from './EditButton';
 
 export default function TeamEditPage() {
+  const mockTeamData = {
+    name: '경영관리',
+    image: '/img_team.png',
+  };
+
   return (
     <div className="mx-auto flex w-[375px] flex-col justify-center px-4 py-14 md:w-[460px] md:py-25">
       <h2 className="text-center text-2xl leading-[28px] font-medium lg:text-[40px] lg:leading-[48px]">
@@ -10,20 +15,12 @@ export default function TeamEditPage() {
       </h2>
 
       <div className="mt-6 flex flex-col gap-6 md:mt-20">
-        <div className="flex flex-col gap-3">
-          <h3 className="text-lg-medium">팀 프로필</h3>
-          <EditableTeamProfile width={64} />
-        </div>
-        <div className="flex flex-col gap-3">
-          <label className="text-lg-medium">팀 이름</label>
-          {/* TODO: name input으로 교체 예정 */}
-          <TextInput />
-        </div>
+        <EditableTeamProfileSection teamProfileUrl={mockTeamData.image} />
+        {/* TODO: Input 교체 후 {mockTeamData.name} 추가 */}
+        <EditableTeamNameSection/>
       </div>
 
-      <Button fullWidth className="mt-10 mb-6">
-        수정하기
-      </Button>
+      <EditButton />
       <p className="text-center text-sm md:text-base">
         팀 이름은 회사명이나 모임 이름 등으로 설정하면 좋아요.
       </p>
