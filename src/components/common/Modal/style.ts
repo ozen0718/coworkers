9f1b9bdf21ee2bd46fec6df0811a72fd44031fad
heresue <sue.synpark@gmail.com>
2025-05-05 04:58:09 +0900

    [Style] 기본 모달 반응형 padding, radius style 수정

--- src/components/common/Modal/style.ts
+++ src/components/common/Modal/style.ts
@@ -2,12 +2,12 @@ export type PaddingStyleType = 'default' | 'todo' | 'danger';
 export type RadiusStyleType = '12' | '24';
 
 export const paddingStyle: Record<PaddingStyleType, string> = {
-  default: 'pt-12 pb-8 px-12 md:px-[52px]',
+  default: 'pt-12 pb-8 px-12 sm:px-[52px]',
   todo: 'py-8 px-6',
-  danger: 'pt-12 md:pt-[42px] pb-8 md:px-[52px]',
+  danger: 'pt-12 sm:pt-[42px] pb-8 sm:px-[52px]',
 };
 
 export const radiusStyle: Record<RadiusStyleType, string> = {
-  '12': 'rounded-t-xl md:rounded-xl',
-  '24': 'rounded-t-3xl md:rounded-3xl',
+  '12': 'rounded-t-xl sm:rounded-xl',
+  '24': 'rounded-t-3xl sm:rounded-3xl',
 };
