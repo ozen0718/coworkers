7da5061a0f82b35753b7b879493487189726cff5
heresue <sue.synpark@gmail.com>
2025-05-06 02:56:57 +0900

    [Refactor] My page layout

--- src/app/(main)/mypage/page.tsx
+++ src/app/(main)/mypage/page.tsx
@@ -11,7 +11,7 @@ export default function MyPage() {
   };
 
   return (
-    <div className="mx-auto flex max-w-[792px] flex-col gap-6 px-4 py-10 md:px-6">
+    <div className="mx-auto flex max-w-[792px] flex-col gap-6 py-10">
       <h2 className="text-2lg-bold">계정 설정</h2>
 
       <EditableProfileSection width={64} />
