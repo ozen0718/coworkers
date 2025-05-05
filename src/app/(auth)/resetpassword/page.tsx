4a904b7ed1384e70a5f39256d36ee6cccd919690
heresue <sue.synpark@gmail.com>
2025-05-06 00:27:55 +0900

    [Refactor] Reset-password page layout

--- src/app/(auth)/resetpassword/page.tsx
+++ src/app/(auth)/resetpassword/page.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import { PasswordInput } from '@/components/common/Inputs';
 import Button from '@/components/common/Button/Button';
 import ResetPasswordRequestModal from '@/components/ResetPw';
+import { PageTitleStyle } from '@/styles/pageStyle';
 
 export default function ResetPasswordPage() {
   const [showModal, setShowModal] = useState(false);
@@ -13,22 +14,26 @@ export default function ResetPasswordPage() {
   }, []);
 
   return (
-    <div className="bg-bg300 flex min-h-screen justify-center px-4">
-      <div className="mt-[160px] mb-[80px] flex w-full max-w-[460px] flex-col gap-8">
-        <h1 className="text-4xl-medium mb-10 text-center text-white">비밀번호 재설정</h1>
+    <>
+      <div className="flex flex-col gap-10">
+        <div>
+          <h1 className={PageTitleStyle}>비밀번호 재설정</h1>
 
-        <div className="flex flex-col gap-2">
-          <label className="text-lg-medium text-white">새 비밀번호</label>
-          <PasswordInput placeholder="비밀번호 (영문, 숫자 포함, 12자 이내)를 입력해주세요." />
-        </div>
+          <div className="flex flex-col gap-6">
+            <div className="flex flex-col gap-3">
+              <label className="text-lg-medium">새 비밀번호</label>
+              <PasswordInput placeholder="비밀번호 (영문, 숫자 포함, 12자 이내)를 입력해주세요." />
+            </div>
 
-        <div className="flex flex-col gap-2">
-          <label className="text-lg-medium text-white">비밀번호 확인</label>
-          <PasswordInput placeholder="새 비밀번호를 다시 한번 입력해주세요." />
+            <div className="flex flex-col gap-3">
+              <label className="text-lg-medium">비밀번호 확인</label>
+              <PasswordInput placeholder="새 비밀번호를 다시 한번 입력해주세요." />
+            </div>
+          </div>
         </div>
 
         <Button size="large" variant="primary" className="mt-6 !w-full">
-          제출하기
+          재설정
         </Button>
       </div>
 
@@ -41,6 +46,6 @@ export default function ResetPasswordPage() {
           setShowModal(false);
         }}
       />
-    </div>
+    </>
   );
 }
