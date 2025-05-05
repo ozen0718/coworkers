6625b1c3544ccc4f7f10a2bdc580ca2b8396a6c9
heresue <sue.synpark@gmail.com>
2025-05-06 05:36:09 +0900

    [Fix] 레이아웃 오류 수정

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,5 +1,7 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="mx-auto max-w-[1200px] px-4 sm:px-6">{children}</div>
+    <div className="px-4 md:px-6">
+      <div className="mx-auto max-w-[1200px]">{children}</div>
+    </div>
   );
 }
