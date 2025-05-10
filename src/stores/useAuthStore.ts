3664c9135c6195170ff66a3861e1ac6011c2bb22
gyoyeon Kim <gy2121@naver.com>
2025-05-10 21:20:01 +0900

    [Merge] upstream/dev/#74

--- src/stores/useAuthStore.ts
+++ src/stores/useAuthStore.ts
@@ -1,7 +1,6 @@
 'use client';
 
 import { create } from 'zustand';
-import { useEffect } from 'react';
 
 interface AuthState {
   accessToken: string | null;
@@ -11,27 +10,6 @@ interface AuthState {
   initializeAuth: () => void;
 }
 
-<<<<<<< HEAD
-export const useAuthStore = create<AuthState>()(
-  persist(
-    (set) => ({
-      accessToken: null,
-      isLoggedIn: false,
-      setAccessToken: (token) =>
-        set(() => {
-          localStorage.setItem('accessToken', token);
-          return { accessToken: token, isLoggedIn: true };
-        }),
-      logout: () =>
-        set(() => {
-          localStorage.removeItem('accessToken');
-          return { accessToken: null, isLoggedIn: false };
-        }),
-    }),
-    {
-      name: 'auth-storage',
-      skipHydration: false,
-=======
 export const useAuthStore = create<AuthState>((set) => ({
   accessToken: null,
   isLoggedIn: false,
@@ -47,7 +25,6 @@ export const useAuthStore = create<AuthState>((set) => ({
     const storedToken = localStorage.getItem('accessToken');
     if (storedToken) {
       set({ accessToken: storedToken, isLoggedIn: true });
->>>>>>> 0cb0444af07e25e2ea36b72e29e86c6c89d6bbd4
     }
   },
 }));
