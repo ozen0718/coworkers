5096a45353df7ea3fa1b229fb71079d0bc974562
heresue <sue.synpark@gmail.com>
2025-05-15 12:19:52 +0900

    [Refactor] useUserInfo 훅에 캡슐화한 api 적용

--- src/hooks/useUserInfo.ts
+++ src/hooks/useUserInfo.ts
@@ -1,9 +1,10 @@
 import { fetchUser } from '@/api/user';
+import { QUERY_KEYS } from '@/constants/queryKeys';
 import { useQuery } from '@tanstack/react-query';
 
 export const useUserInfo = () => {
   const query = useQuery({
-    queryKey: ['userInfo'],
+    queryKey: QUERY_KEYS.user.me,
     queryFn: fetchUser,
     retry: false, // 로그인 안 된 상태에서 401 응답 받았을 때 무한 재시도 방지
   });
