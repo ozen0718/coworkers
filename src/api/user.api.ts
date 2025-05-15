c18559a2b83101ad7d43f52625565de709384785
doctor-taco <100111506+doctor-taco@users.noreply.github.com>
2025-05-15 15:07:55 +0900

    [Chore] Merge duplicated api folders into one

--- src/api/user.api.ts
+++ src/api/user.api.ts
@@ -1,4 +1,4 @@
-import axiosInstance from '@/app/api/axiosInstance';
+import axiosInstance from '@/api/axiosInstance';
 
 export const getCurrentUser = async () => {
   const response = await axiosInstance.get('/user');
--- src/hooks/useCurrentUser.ts
+++ src/hooks/useCurrentUser.ts
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import { getCurrentUser } from '@/app/api/user.api';
+import { getCurrentUser } from '@/api/user.api';
 import { UserResponse } from '@/types/usertypes';
 
 export const useCurrentUser = () => {
