24a9fae59069c23196f3023f8ac1e5aeaaea60f3
heresue <sue.synpark@gmail.com>
2025-05-15 11:47:17 +0900

    [Refactor] fetchUser API 캡슐화 및 iseLoggedIn, isAdmin 로직 추가

--- src/hooks/useUserInfo.ts
+++ src/hooks/useUserInfo.ts
@@ -1,13 +1,16 @@
-import axiosInstance from '@/app/api/axiosInstance';
+import { fetchUser } from '@/api/user';
 import { useQuery } from '@tanstack/react-query';
 
 export const useUserInfo = () => {
-  return useQuery({
+  const query = useQuery({
     queryKey: ['userInfo'],
-    queryFn: async () => {
-      const { data } = await axiosInstance.get('/user');
-      return data;
-    },
+    queryFn: fetchUser,
     retry: false, // 로그인 안 된 상태에서 401 응답 받았을 때 무한 재시도 방지
   });
+
+  return {
+    ...query,
+    isLoggedIn: !!query.data?.id,
+    isAdmin: query.data?.role === 'ADMIN',
+  };
 };
