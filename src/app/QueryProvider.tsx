180f3cf7e1931d19aa24aef03a608dcb7e6e09c3
heresue <sue.synpark@gmail.com>
2025-05-15 11:54:19 +0900

    [Feat] React Query Devtools 적용

--- src/app/QueryProvider.tsx
+++ src/app/QueryProvider.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import { ReactNode, useState } from 'react';
 
 export default function QueryProvider({ children }: { children: ReactNode }) {
@@ -22,5 +24,10 @@ export default function QueryProvider({ children }: { children: ReactNode }) {
       })
   );
 
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
