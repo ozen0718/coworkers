dc32a3dbfd0af95643a23ed6890a5f0b1f4be7c2
gyoyeon Kim <gy2121@naver.com>
2025-05-16 22:19:21 +0900

    [Test] 임시로 alret으로 변경

--- src/components/Card/Post/PostCard.tsx
+++ src/components/Card/Post/PostCard.tsx
@@ -13,6 +13,8 @@ import { deleteArticle, fetchArticle } from '@/api/articles';
 import { useQueryClient } from '@tanstack/react-query';
 import { useQuery } from '@tanstack/react-query';
 import { useEffect } from 'react';
+import { getUserInfo } from '@/api/user';
+import { toast, ToastContainer } from 'react-toastify';
 
 const sizeClass = {
   large: 'min-h-[176px] w-full',
@@ -51,13 +53,32 @@ export default function PostCard({
     setIsDropDownOpen((prev) => !prev);
   };
 
+  /* 사용자 정보 */
+  const { data: userInfo } = useQuery({
+    queryKey: ['userInfo'],
+    queryFn: getUserInfo,
+  });
+
+  let isWriter = false;
+  if (userInfo?.id && writer?.id) {
+    isWriter = userInfo.id === writer.id;
+  }
+
   /* Dropdown 게시글 수정 */
   const handleEdit = () => {
+    if (!isWriter) {
+      toast.error('작성자만 수정할 수 있습니다');
+      return;
+    }
     router.push(`/boards/${id}/edit`);
   };
 
   /* Dropdown 게시글 삭제 */
   const handleDelete = async () => {
+    if (!isWriter) {
+      alert('작성자만 삭제할 수 있습니다');
+      return;
+    }
     deleteMutate();
   };
 
@@ -124,107 +145,114 @@ export default function PostCard({
   }, [postData?.data?.isLiked, postData?.data?.likeCount]);
 
   return (
-    <div
-      className={clsx(
-        'border-bg100 bg-bg200 flex w-full flex-col gap-1.5 rounded-xl border p-5',
-        type === 'best' ? 'max-h-[220px] w-full' : sizeClass[size]
-      )}
-    >
-      {/* 헤더 */}
-      {type === 'best' && (
-        <div className="mr-4 flex">
-          <Image src="/icons/best.svg" alt="베스트 아이콘" width={16} height={16} />
-          <p className="text-md-semibold ml-1">Best</p>
-        </div>
-      )}
+    <>
+      <ToastContainer />
+      <div
+        className={clsx(
+          'border-bg100 bg-bg200 flex w-full flex-col gap-1.5 rounded-xl border p-5',
+          type === 'best' ? 'max-h-[220px] w-full' : sizeClass[size]
+        )}
+      >
+        {/* 헤더 */}
+        {type === 'best' && (
+          <div className="mr-4 flex">
+            <Image src="/icons/best.svg" alt="베스트 아이콘" width={16} height={16} />
+            <p className="text-md-semibold ml-1">Best</p>
+          </div>
+        )}
 
-      {/* 내용 */}
-      <div className="flex w-full cursor-pointer items-start">
-        <div
-          className="relative flex w-full items-start justify-between"
-          onClick={handleTitleClick}
-        >
-          <p
-            className={clsx(
-              'mr-2 line-clamp-2 pr-6',
-              type === 'best' ? 'text-sm leading-[24px] sm:text-lg' : fontClass[size]
-            )}
-          >
-            {title}
-          </p>
-
+        {/* 내용 */}
+        <div className="flex w-full cursor-pointer items-start">
           <div
-            className={clsx(
-              'relative',
-              type === 'best' ? 'min-w-[72px]' : size === 'small' ? 'min-w-[72px]' : 'min-w-[112px]'
-            )}
-            onClick={(e) => e.stopPropagation()} // 부모 이벤트 막기
+            className="relative flex w-full items-start justify-between"
+            onClick={handleTitleClick}
           >
-            {image ? (
-              <Image
-                className="aspect-square min-w-[72px] rounded-lg object-cover"
-                src={image}
-                alt="게시글 이미지"
-                width={72}
-                height={72}
-                sizes="(max-width: 600px) 50vw, 72px"
-              />
-            ) : (
-              <div className="aspect-square h-[72px] w-[72px] min-w-[72px] rounded-lg bg-transparent" />
-            )}
+            <p
+              className={clsx(
+                'mr-2 line-clamp-2 pr-6',
+                type === 'best' ? 'text-sm leading-[24px] sm:text-lg' : fontClass[size]
+              )}
+            >
+              {title}
+            </p>
 
-            {type === 'general' && topshowKebab && (
-              <Image
-                className="absolute top-0 right-0 cursor-pointer"
-                src="/icons/kebab.svg"
-                alt="옵션"
-                width={24}
-                height={24}
-                onClick={(e) => {
-                  e.stopPropagation();
-                  toggleDropdown();
-                }}
+            <div
+              className={clsx(
+                'relative',
+                type === 'best'
+                  ? 'min-w-[72px]'
+                  : size === 'small'
+                    ? 'min-w-[72px]'
+                    : 'min-w-[112px]'
+              )}
+              onClick={(e) => e.stopPropagation()} // 부모 이벤트 막기
+            >
+              {image ? (
+                <Image
+                  className="aspect-square min-w-[72px] rounded-lg object-cover"
+                  src={image}
+                  alt="게시글 이미지"
+                  width={72}
+                  height={72}
+                  sizes="(max-width: 600px) 50vw, 72px"
+                />
+              ) : (
+                <div className="aspect-square h-[72px] w-[72px] min-w-[72px] rounded-lg bg-transparent" />
+              )}
+
+              {type === 'general' && topshowKebab && (
+                <Image
+                  className="absolute top-0 right-0 cursor-pointer"
+                  src="/icons/kebab.svg"
+                  alt="옵션"
+                  width={24}
+                  height={24}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    toggleDropdown();
+                  }}
+                />
+              )}
+            </div>
+            {isDropDownOpen && (
+              <PostDropdown
+                type="kebab"
+                textJustify="center"
+                onClick={(e) => e.stopPropagation()}
+                options={[
+                  { label: '수정하기', value: '수정', action: handleEdit },
+                  { label: '삭제하기', value: '삭제', action: handleDelete },
+                ]}
+                isOpen={isDropDownOpen}
+                toggleDropdown={toggleDropdown}
+                toppercent="30%"
               />
             )}
           </div>
-          {isDropDownOpen && (
-            <PostDropdown
-              type="kebab"
-              textJustify="center"
-              onClick={(e) => e.stopPropagation()}
-              options={[
-                { label: '수정하기', value: '수정', action: handleEdit },
-                { label: '삭제하기', value: '삭제', action: handleDelete },
-              ]}
-              isOpen={isDropDownOpen}
-              toggleDropdown={toggleDropdown}
-              toppercent="30%"
-            />
-          )}
+        </div>
+
+        {/* 날짜 */}
+        {(type === 'best' || (type === 'general' && size === 'small')) && (
+          <p className={clsx('text-xs-medium text-gray400 mt-2', type === 'best' && 'mt-4')}>
+            {date}
+          </p>
+        )}
+
+        {/* 작성자 정보 */}
+        <div>
+          <AuthorInfo
+            showKebab={showKebab && size === 'small'}
+            showDate={type === 'general' && size !== 'small' ? true : false}
+            showDivider={type === 'general' && size !== 'small' ? true : false}
+            likeCount={likeCountState}
+            authorName={writer?.nickname}
+            date={date}
+            articleId={id}
+            isLiked={isLikedState}
+            onLikeChanged={handleLikeChanged}
+          />
         </div>
       </div>
-
-      {/* 날짜 */}
-      {(type === 'best' || (type === 'general' && size === 'small')) && (
-        <p className={clsx('text-xs-medium text-gray400 mt-2', type === 'best' && 'mt-4')}>
-          {date}
-        </p>
-      )}
-
-      {/* 작성자 정보 */}
-      <div>
-        <AuthorInfo
-          showKebab={showKebab && size === 'small'}
-          showDate={type === 'general' && size !== 'small' ? true : false}
-          showDivider={type === 'general' && size !== 'small' ? true : false}
-          likeCount={likeCountState}
-          authorName={writer?.nickname}
-          date={date}
-          articleId={id}
-          isLiked={isLikedState}
-          onLikeChanged={handleLikeChanged}
-        />
-      </div>
-    </div>
+    </>
   );
 }
