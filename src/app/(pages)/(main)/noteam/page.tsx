13a976413f8d659bf401acceb3927d543463923f
ozen0718 <limyong301p@naver.com>
2025-05-25 04:05:17 +0000

    [Feat] No-team page 진입 시 팀이 있을 경우 팀페이지로 라우팅

--- src/app/(pages)/(main)/noteam/page.tsx
+++ src/app/(pages)/(main)/noteam/page.tsx
@@ -1,24 +1,32 @@
-import Button from '@/components/common/Button/Button';
-import Image from 'next/image';
-import Link from 'next/link';
+'use client';
 
-export default function noTeamPage() {
-  return (
-    <div className="mx-auto flex h-[calc(100vh-60px)] max-w-[560px] min-w-[375px] flex-col items-center justify-center px-[14px] md:min-w-[520px] md:px-0 lg:min-w-[810px]">
-      <Image src="images/noTeam.svg" width={810} height={255} alt="no team image" />
-      <p className="text-gray500 mt-8 mb-12 text-center text-[14px] leading-[17px] font-medium md:mt-12 md:mb-20 md:text-[16px] md:leading-[19px]">
-        아직 소속됨 팀이 없습니다.
-        <br />
-        팀을 생성하거나 팀에 참여해보세요.
-      </p>
-      <div className="flex w-[186px] flex-col gap-4">
-        <Button fullWidth>
-          <Link href="/addteam">팀 생성하기</Link>
-        </Button>
-        <Button fullWidth variant="inverse" className="!bg-transparent">
-          <Link href="/join">팀 참여하기</Link>
-        </Button>
-      </div>
-    </div>
-  );
+import NoTeamPage from './NoTeam';
+import { useEffect } from 'react';
+import { useUserStore } from '@/stores/useUserStore';
+import { useAuthStore } from '@/stores/useAuthStore';
+import { useRouter } from 'next/navigation';
+
+export default function NoTeamPageWrapper() {
+  const { teams, isInitialized } = useUserStore();
+  const { accessToken } = useAuthStore();
+  const router = useRouter();
+
+  console.log(accessToken)
+
+  useEffect(() => {
+    if (!isInitialized) return;
+
+    if (!accessToken) {
+      router.replace('/login');
+      return;
+    }
+
+    if (teams.length > 0) {
+      router.replace(`/${teams[0].id}`);
+    }
+  }, [accessToken, isInitialized, teams, router]);
+
+  if (!isInitialized || !accessToken) return null;
+
+  return <NoTeamPage />;
 }
